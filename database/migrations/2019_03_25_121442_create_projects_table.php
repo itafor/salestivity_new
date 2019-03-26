58da4b81a6a7aef1fad1a6bdc48dfaa0ce67cbe4
seunsekoni <sekonioluwaseun@gmail.com>
2019-03-26 11:49:08 +0100

    Created the database and all migrations

---
+++ database/migrations/2019_03_25_121442_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('customer_account');
+            $table->string('product');
+            $table->string('technician');
+            $table->string('notes');
+            $table->string('uploads');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
