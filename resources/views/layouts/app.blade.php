a38fba14356fbb953ea74dc4ae42162e372a1f66
francis <itaforfrancis@gmail.com>
2020-01-25 02:23:51 -0800

    mainjs file created for all scripts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -118,66 +118,8 @@
     }
 })();
 
-
-    let product_price =0;
-        $('#product_id').change(function(){
-            var product_id = $(this).val();
-            if(product_id){
-                $.ajax({
-                    url: baseUrl+'/fetch-product-price/'+product_id,
-                    type: "GET",
-                    dataType: 'json',
-                    success: function(data) {
-                        console.log(data.products.standard_price)
-                        $('#productPrice').empty();
-                        product_price=data.products.standard_price;
-                        $('#productPrice').val(data.products.standard_price);
-                    }
-                });
-            }
-            else{
-                $('#productPrice').val('');
-                product_price=0;
-            }
-        });
-
-
-        $('body').on('keyup', '#discount', function(){
-
-            let discount = $(this).val();
-           if(0 < discount && discount < 101){
-            if(parseFloat(product_price) <= 0){
-               alert('Please select a product to display product price')
-               $('#discount').val('');
-            }else{
-                let billingAmount = (discount/100) * product_price;
-              $('#billingAmount').val(billingAmount)
-            }
-        }else{
-             $('#billingAmount').val('');
-            alert('discount must not be more than 100')
-        }
-        })
-
-$(document).on('keyup', '#discount', function(e){
-    e.preventDefault();
-    let value = e.target.value;
-    //alert(value)
-if(value <= 0){
-     $(this).val('');
-    
-}
- });
-$(document).on('keyup', '#productPrice', function(e){
-    e.preventDefault();
-    let value = e.target.value;
-    //alert(value)
-if(value <= 0){
-     $(this).val('');
-}
- });
-
-
         </script>
+    <script src="{{url('js/mainjs.js')}}"></script>
+
     </body>
 </html>
