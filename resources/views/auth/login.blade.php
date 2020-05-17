bf3a58957f493b1da279127ef84fb71efe9b9e3a
femi <wm.netscope@gmail.com>
2020-05-17 07:48:15 +0100

    Dashboard update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app', ['class' => 'bg-default'])
 
 @section('content')
-    <div class="header bg-solid-custom py-7 py-lg-8">
+<div class="header bg-solid-custom py-7 py-lg-8">
     <div class="container">
         <div class="header-body text-center mb-7">
             <div class="row justify-content-center">
