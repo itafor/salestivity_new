4fd686f779e68b63b658c76cdd3e5a6f5ec2ee24
francis <itaforfrancis@gmail.com>
2021-01-28 03:21:28 -0800

    loader added

--- resources/views/billing/renewal/create.blade.php
+++ resources/views/billing/renewal/create.blade.php
@@ -234,7 +234,20 @@
 
 <div class="text-center">
     <button onclick="removeDisabledAttr()" type="submit" class="btn btn-success mt-4" id="submitRenewalButton">{{ __('Save') }}</button>
-   <img src="{{URL::asset('/img/ajax-loader.gif')}}" alt="spinner" style="display: none;" id="loader">
+  <div class="sk-circle" id="loader">
+  <div class="sk-circle1 sk-child"></div>
+  <div class="sk-circle2 sk-child"></div>
+  <div class="sk-circle3 sk-child"></div>
+  <div class="sk-circle4 sk-child"></div>
+  <div class="sk-circle5 sk-child"></div>
+  <div class="sk-circle6 sk-child"></div>
+  <div class="sk-circle7 sk-child"></div>
+  <div class="sk-circle8 sk-child"></div>
+  <div class="sk-circle9 sk-child"></div>
+  <div class="sk-circle10 sk-child"></div>
+  <div class="sk-circle11 sk-child"></div>
+  <div class="sk-circle12 sk-child"></div>
+</div>
 </div>
 
 </form>
