8cc61eb13f41f7372915f4b5349858397b68d29f
itafor <itaforfrancis@gmail.com>
2021-06-03 10:47:22 -0700

    create and view email marketing link commented

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -386,7 +386,7 @@
                         <span class="nav-link-text">{{ __('Email Marketing') }}</span>
                     </a>
 
-                    <div class="collapse" id="navbar-email_marketing">
+                <!--     <div class="collapse" id="navbar-email_marketing">
                         <ul class="nav nav-sm flex-column">
                             <ul class="nav nav-sm flex-column">
                                 <li class="nav-item">
@@ -401,7 +401,7 @@
                                 </li>
                             </ul>
                         </ul>
-                    </div>
+                    </div> -->
                 </li>
 
                  <li class="nav-item">
