4ec8557f870fdca787c9283a6b7c8ac7439f6cbb
itafor <itaforfrancis@gmail.com>
2021-09-08 11:20:36 -0700

    Normal Invoice subject changd to Invoice Notification

--- app/Mail/SendInvoice.php
+++ app/Mail/SendInvoice.php
@@ -32,23 +32,23 @@ class SendInvoice extends Mailable
     public function build()
     {
         $pdf = PDF::loadView('emails.sendinvoice', [
-            'invoice'=> $this->invoice, 
+            'invoice'=> $this->invoice,
         ]);
 
         $documentName = 'invoicePayment_'.$this->invoice->invoice_number.'.pdf';
 
         
-      $text = view('whatsapp.sendinvoice', [
-            'invoice'=> $this->invoice, 
+        $text = view('whatsapp.sendinvoice', [
+            'invoice'=> $this->invoice,
         ]);
 
         whatsappNotification('14157386170', '2347065907948', strip_tags($text));
     
         return $this->view('emails.sendinvoice')
-         ->replyTo('billing@digitalweb247.com','Digitalweb247')
+         ->replyTo('billing@digitalweb247.com', 'Digitalweb247')
         ->attachData($pdf->output(), $documentName)
-        ->subject('Invoice Payment Notification')
-        ->cc('billing@digitalweb247.com','Digitalweb247');
-         // ->cc($this->invoice->compEmail ? $this->invoice->compEmail->email : $this->invoice->user->email);
+        ->subject('Invoice Notification')
+        ->cc('billing@digitalweb247.com', 'Digitalweb247');
+        // ->cc($this->invoice->compEmail ? $this->invoice->compEmail->email : $this->invoice->user->email);
     }
 }
