1a5d6c979b8c644cd1ac5409e6ae5e1d938f4870
itafor <itaforfrancis@gmail.com>
2021-10-14 21:10:48 -0700

    renewExpiredReccuringByOneYear first, second and third renewal durations assigned default value

--- app/Http/Controllers/CronJobController.php
+++ app/Http/Controllers/CronJobController.php
@@ -128,9 +128,9 @@ public static function notifyCustomer($renewal){
         $discountedPrice = ($discountValue / 100) * $renewal->productPrice;
         $finalPrice = $renewal->productPrice - $discountedPrice;
 
-        // $data['first_duration'] = $renewal->duration ->first_duration;
-        // $data['second_duration'] = $renewal->duration->second_duration;
-        // $data['third_duration'] = $renewal->duration->third_duration;
+        $data['first_duration'] = 60; // $renewal->duration->first_duration;
+        $data['second_duration'] = 30; //$renewal->duration->second_duration;
+        $data['third_duration'] = 7;  //$renewal->duration->third_duration;
 
         $new_renewal = new Renewal();
         $new_renewal->main_acct_id = $renewal->main_acct_id;
